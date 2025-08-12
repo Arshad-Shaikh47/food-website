621fc4616039c11daf13a9f4f610accdf58a0c28
Arshad-Shaikh47 <shaikhaarshad47@gmail.com>
2025-08-12 19:00:52 +0530

    update img

--- src/componants/Header/Header.jsx
+++ src/componants/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 const Header = () => {
   return (
     <div className='header'>
+      <img src={assets.header_img} alt="" />
        <div className='header-contents'>
         <h2>Order your favourite here </h2>
         <p>choose from a diverse menu fuaturing a delectable arry  of dishes. Our mission is to satisfy your cravings and elevate your dining experience . One delicious meal at a time. </p>
